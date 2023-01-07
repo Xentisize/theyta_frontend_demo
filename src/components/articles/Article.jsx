624f50131244cb643761216a2df9fd113f14dff2
Xentisize <xentisize@gmail.com>
2023-01-07 17:10:08 +0800

    fix caption on 3d chart

--- src/components/articles/Article.jsx
+++ src/components/articles/Article.jsx
@@ -177,7 +177,7 @@ const Article = () => {
 
 					<div className="h-72 basis-auto rounded-lg sm:basis-1/3">
 						<babylon model="/chart01.glb"></babylon>
-						<p className="py-2 indent-3 text-sm italic text-slate-500">
+						<p className="indent-3 text-sm italic text-slate-500">
 							3D Visualization to the Growth of Private Domestic Houses
 						</p>
 					</div>
